debb4abff25cd7b580ba6afb5ce7b6f43b387419
Roman Jones <rjon141@lsu.edu>
2019-01-25 22:00:46 -0600

    Picking up items (still has bugs)

--- GlobalGameJam2019/Assets/Scripts/PlayerController.cs
+++ GlobalGameJam2019/Assets/Scripts/PlayerController.cs
@@ -13,6 +13,26 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField]
     private float turnSpeed;
+
+    [SerializeField]
+    private Transform carryPointN;
+
+    [SerializeField]
+    private Transform carryPointS;
+
+    [SerializeField]
+    private Transform carryPointE;
+
+    [SerializeField]
+    private Transform carryPointW;
+
+    // Store the carry object that we last collided with
+    private GameObject carryObject;
+
+    private bool isCarrying;
+
+    private string idleDirection;
+
     #endregion
 
     private void Start()
@@ -24,26 +44,76 @@ public class PlayerController : MonoBehaviour
     {
         while (true)
         {
-            Vector2 inputDirection = Vector2.zero;
+           Vector2 inputDirection = Vector2.zero;
+
+            if (Input.GetKey(KeyCode.Space) && carryObject != null)
+            {
+                isCarrying = !isCarrying;
+                carryObject.transform.localPosition = Vector2.zero;
+            }
 
             if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-                inputDirection += Vector2.up;
+            {
+                if (isCarrying)
+                {
+                    carryObject.transform.parent = carryPointN;
+                    carryObject.transform.localPosition = Vector2.zero;
+                }
 
+                inputDirection += Vector2.up;
+                idleDirection = "up";
+            }
             if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                if (isCarrying)
+                {
+                    carryObject.transform.parent = carryPointW;
+                    carryObject.transform.localPosition = Vector2.zero;
+                }
+
                 inputDirection += Vector2.left;
+                idleDirection = "left";
+            }
 
             if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                if (isCarrying)
+                {
+                    carryObject.transform.parent = carryPointE;
+                    carryObject.transform.localPosition = Vector2.zero;
+                }
+
                 inputDirection += Vector2.right;
+                idleDirection = "right";
+            }
 
             if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            {
+                if (isCarrying)
+                {
+                    carryObject.transform.parent = carryPointS;
+                    carryObject.transform.localPosition = Vector2.zero;
+                }
+
                 inputDirection += Vector2.down;
+                idleDirection = "down";
+            }
 
             inputDirection.Normalize();
             inputDirection *= movementSpeed;
 
             rb.velocity = Vector2.Lerp(rb.velocity, inputDirection, turnSpeed);
 
+
             yield return new WaitForFixedUpdate();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Food") && !isCarrying)
+        {
+            carryObject = collision.gameObject;
         }
     }
 }
